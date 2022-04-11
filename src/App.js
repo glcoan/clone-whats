e3933c682555fed14ae181d5e3db37f04e88d8f0
Gabriel - Home <g.coan012@gmail.com>
2022-04-11 11:14:11 -0300

    Visual do header sidebar completo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
 
-export default App;
+export default () => {
+    return (
+        <>
+            <div className="app-window">
+                <div className="sidebar">
+                    
+                    <header>
+                        <img className="header--avatar" src="https://www.w3schools.com/w3images/avatar2.png" alt="" />
+                            <div className="header--buttons">
+                                <div className="header--btn">
+                                    <DonutLargeIcon style={({color: "#919191"})} />
+                                </div>
+                                <div className="header--btn">
+                                    <ChatIcon style={({color: "#919191"})} />
+                                </div>
+                                <div className="header--btn">
+                                    <MoreVertIcon style={({color: "#919191"})} />
+                                </div>
+                            </div>
+                    </header>
+
+                    <div className="search">
+                        <div className="search--input">
+                            <SearchIcon fontSize="small" style={({color: "#919191"})} />
+                            <input type="search" placeholder="Procurar ou começar uma nova conversa" />
+                        </div>
+                    </div>
+
+                    <div className="chatlist">
+                        ...
+                    </div>
+
+                </div>
+                <div className="contentarea">
+                    ...
+                </div>
+            </div>    
+        </>
+    );
+}
