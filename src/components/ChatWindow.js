6b97622e8a67152f8aec80110a57ee04ac974fed
Gabriel - Work <gabriel.coan@pedzap.com.br>
2022-04-13 14:06:32 -0300

    Commit teste - removendo 1 log

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -45,7 +45,6 @@ export default ({ data }) => {
             }
             recognition.onresult = (e) => {
                 setText( e.results[0][0].transcript );
-                console.log(e.results[0][0].transcript);
             }
 
             recognition.start();
